617957a48b76f41a48f100d9079ddbaa44812d1b
eduairet <hola@eduairet.com>
2023-06-12 23:52:10 -0600

    Cors fix

--- server/index.js
+++ server/index.js
@@ -14,6 +14,6 @@ const corsUrl = process.env.CORS_ORIGIN,
     };
 
 app.use(morgan('tiny'), express.static('./public'));
-app.use(cors(corsOptions));
+app.use(cors('*'));
 app.use('/', sketch);
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
