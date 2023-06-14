74cbf529d071db3c0100f9c32f95c9bc57a9c362
eduairet <hola@eduairet.com>
2023-06-13 19:55:43 -0600

    Cors fix changes

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const express = require('express'),
     sketch = require('./routes/sketch-route');
 
 require('dotenv').config();
-const { CORS_ORIGIN, SERVER_ORIGIN } = process.env,
+const { CORS_ORIGIN } = process.env,
     corsOptions = {
-        origin: [CORS_ORIGIN, SERVER_ORIGIN],
+        origin: CORS_ORIGIN,
     };
 
 app.use(morgan('tiny'), express.static('./public'));
