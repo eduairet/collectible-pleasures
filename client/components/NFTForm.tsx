76da7f792448b0e381b622c74192790ec7fdf81e
eduairet <hola@eduairet.com>
2023-06-14 19:15:49 -0600

    UI adjustments

--- client/components/NFTForm.tsx
+++ client/components/NFTForm.tsx
@@ -16,9 +16,12 @@ export default function NFTForm() {
                 handleMint();
             }}
         >
-            <label htmlFor='nft' className='text-center text-lg'>
-                Type your 3 letter NFT name
-            </label>
+            <div className='flex flex-col'>
+                <label htmlFor='nft' className='text-center text-lg'>
+                    Type your 3 letter NFT name
+                </label>
+                <p className='text-center text-sm italic'>(A-Z letters and space only)</p>
+            </div>
             <input
                 id='nft'
                 className='text-center text-2xl rounded-md p-2 w-full black text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-75'
--- client/components/Preview.tsx
+++ client/components/Preview.tsx
@@ -7,7 +7,7 @@ export default function Preview() {
     const nftCtx = useContext(NftContext);
 
     return (
-        <div className="w-full h-[350px] mx-auto flex flex-col items-center justify-center container overflow-hidden border-2 border-white rounded-lg">
+        <div className="w-full h-[350px] mx-auto flex flex-col items-center justify-center container overflow-hidden rounded-lg">
             {nftCtx ? (nftCtx.isLoading && !nftCtx.error ? <PreviewSpinner /> : nftCtx.error && !nftCtx.isLoading ? <Error errorMessage={nftCtx.error} /> :
                 <object width="350" height="350" data={nftCtx.url}>
                 </object>) : null}
